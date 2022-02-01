24f3d77fde6ebeb824285018218750ea57441d73
Mózes Szabó <mozes.szabo@webvalto.hu>
2022-02-01 15:30:59 +0100

    collections 1

--- collection-interfaces/src/test/java/collection/interfaces/Exercise1Test.java
+++ collection-interfaces/src/test/java/collection/interfaces/Exercise1Test.java
@@ -21,7 +21,8 @@ import static org.junit.Assert.*;
 
 public class Exercise1Test extends ClassicOnlineStore {
 
-    @Easy @Test
+    @Easy
+    @Test
     public void iterateByForEach() {
         Iterable<Customer> customerIterable = this.mall.getCustomerList();
         List<String> nameList = new ArrayList<>();
@@ -31,80 +32,85 @@ public class Exercise1Test extends ClassicOnlineStore {
          * Iterate {@link customerIterable} with {@link Iterable#forEach} and use the {@link Consumer}
          * to finish creating the name list.
          */
-        Consumer<Object> consumer = null;
-        // customerIterable.
+        Consumer<Customer> consumer = (customer) -> nameList.add(customer.getName());
+        customerIterable.forEach(consumer);
 
         assertThat(nameList.toString(), is("[Joe, Steven, Patrick, Diana, Chris, Kathy, Alice, Andrew, Martin, Amy]"));
     }
 
-    @Easy @Test
+    @Easy
+    @Test
     public void whoHaveNoEInYourName() {
         Collection<String> nameCollection =
-            new ArrayList<>(Arrays.asList("Joe", "Steven", "Patrick", "Chris"));
+                new ArrayList<>(Arrays.asList("Joe", "Steven", "Patrick", "Chris"));
 
         /**
          * Create a {@link Predicate} which predicates if the input string contains "e".
          * Remove elements from {@link nameCollection} which contains "e" using {@link Collection#removeIf}.
          */
-        Predicate<Object> predicate = null;
-        // nameCollection.
+        Predicate<String> predicate = (name) -> name.contains("e");
+        nameCollection.removeIf(predicate);
 
         assertThat(nameCollection.toString(), is("[Patrick, Chris]"));
     }
 
-    @Easy @Test
+    @Easy
+    @Test
     public void replaceTheElements() {
         List<String> nameList =
-            new ArrayList<>(Arrays.asList("Joe", "Steven", "Patrick", "Chris"));
+                new ArrayList<>(Arrays.asList("Joe", "Steven", "Patrick", "Chris"));
 
         /**
          * Create a {@link UnaryOperator} which returns given string wrapped with "()".
          * Replace the elements in {@link nameList} with string wrapped with "()" using {@link List#replaceAll} .
          */
-        UnaryOperator<Object> unaryOperator = null;
-        // nameList.
+        UnaryOperator<String> unaryOperator = (string) -> "(" + string + ")";
+        nameList.replaceAll(unaryOperator);
 
         assertThat(nameList.toString(), is("[(Joe), (Steven), (Patrick), (Chris)]"));
     }
 
-    @Easy @Test
+    @Easy
+    @Test
     public void sortByName() {
         List<String> nameList =
-            new ArrayList<>(Arrays.asList("Joe", "Steven", "Patrick", "Chris"));
+                new ArrayList<>(Arrays.asList("Joe", "Steven", "Patrick", "Chris"));
 
         /**
          * Create a {@link Comparator} to sort the name list by their name's length in ascending order.
          */
-        Comparator<Object> comparator = null;
-        // nameList.
+        Comparator<String> comparator = Comparator.comparingInt(String::length);
+        nameList.sort(comparator);
 
         assertThat(nameList.toString(), is("[Joe, Chris, Steven, Patrick]"));
     }
 
-    @Easy @Test
+    @Easy
+    @Test
     public void createStream() {
         Collection<String> nameList =
-            new ArrayList<>(Arrays.asList("Joe", "Steven", "Patrick", "Chris"));
+                new ArrayList<>(Arrays.asList("Joe", "Steven", "Patrick", "Chris"));
 
         /**
          * Create a serial {@link Stream} using {@link Collection#stream}
          * You can learn about {@link Stream} APIs at stream-api module.
          */
-        Stream<Object> nameStream = null;
+        Stream<String> nameStream = nameList.stream();
 
         assertThat(nameStream.count(), is(4L));
         assertThat(nameStream.isParallel(), is(false));
     }
 
-    @Easy @Test
+    @Easy
+    @Test
     public void createParallelStream() {
         Collection<String> nameList =
-            new ArrayList<>(Arrays.asList("Joe", "Steven", "Patrick", "Chris"));
+                new ArrayList<>(Arrays.asList("Joe", "Steven", "Patrick", "Chris"));
 
         /**
          * Create a parallel {@link Stream} using {@link Collection#parallelStream} or {@link Stream#parallel}
          */
-        Stream<Object> nameParallelStream = null;
+        Stream<String> nameParallelStream = nameList.parallelStream();
 
         assertThat(nameParallelStream.count(), is(4L));
         assertThat(nameParallelStream.isParallel(), is(true));
