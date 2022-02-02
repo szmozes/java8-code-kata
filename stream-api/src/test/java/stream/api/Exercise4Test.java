d91d0bb0c16bf65363a43d96d09923c89885e945
Mózes Szabó <mozes.szabo@webvalto.hu>
2022-02-02 12:51:47 +0100

    stream-api 4

--- stream-api/src/test/java/stream/api/Exercise4Test.java
+++ stream-api/src/test/java/stream/api/Exercise4Test.java
@@ -15,7 +15,8 @@ import static org.junit.Assert.*;
 
 public class Exercise4Test extends ClassicOnlineStore {
 
-    @Easy @Test
+    @Easy
+    @Test
     public void firstRegistrant() {
         List<Customer> customerList = this.mall.getCustomerList();
 
@@ -23,36 +24,42 @@ public class Exercise4Test extends ClassicOnlineStore {
          * Find the first customer who registered this online store by using {@link Stream#findFirst}
          * The customerList are ascending ordered by registered timing.
          */
-        Optional<Customer> firstCustomer = null;
+        Optional<Customer> firstCustomer = customerList.stream()
+                .findFirst();
 
         assertThat(firstCustomer.get(), is(customerList.get(0)));
     }
 
-    @Easy @Test
+    @Easy
+    @Test
     public void isThereAnyoneOlderThan40() {
         List<Customer> customerList = this.mall.getCustomerList();
 
         /**
          * Check whether any customer older than 40 exists or not, by using {@link Stream#anyMatch}
          */
-        boolean olderThan40Exists = true;
+        boolean olderThan40Exists = customerList.stream()
+                .anyMatch(c -> c.getAge() > 40);
 
         assertThat(olderThan40Exists, is(false));
     }
 
-    @Easy @Test
+    @Easy
+    @Test
     public void isEverybodyOlderThan20() {
         List<Customer> customerList = this.mall.getCustomerList();
 
         /**
          * Check whether all customer are older than 20 or not, by using {@link Stream#allMatch}
          */
-        boolean allOlderThan20 = false;
+        boolean allOlderThan20 = customerList.stream()
+                .allMatch(c -> c.getAge() > 20);
 
         assertThat(allOlderThan20, is(true));
     }
 
-    @Easy @Test
+    @Easy
+    @Test
     public void everyoneWantsSomething() {
         List<Customer> customerList = this.mall.getCustomerList();
 
@@ -60,7 +67,8 @@ public class Exercise4Test extends ClassicOnlineStore {
          * Confirm that none of the customer has empty list for their {@link Customer.wantToBuy}
          * by using {@link Stream#noneMatch}
          */
-        boolean everyoneWantsSomething = false;
+        boolean everyoneWantsSomething = customerList.stream()
+                .noneMatch(c-> c.getWantToBuy().isEmpty());
 
         assertThat(everyoneWantsSomething, is(true));
     }
